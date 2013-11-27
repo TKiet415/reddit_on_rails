0f84f63f1707adc8bae295cb3ca7e51bb8470513
Brian Pham <phambrian@live.com>
2013-11-26 16:03:49 -0800

    Initial commit before devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditOnRails::Application.config.secret_key_base = '742e62f4fcacc9709d790ff6430e48569b2eda386788ad116d1b21bb41864a032e1018bbcb80c0dafd6ddb36f1b672c98f78d2a24f6beab86fa6aecd79294d9f'
