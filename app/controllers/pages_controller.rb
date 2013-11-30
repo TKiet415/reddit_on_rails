fc4a0de719c2921e947e508e5eca0f983bb5fbd1
Brian Pham <phambrian@live.com>
2013-11-30 00:59:28 -0800

    Comments link template added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
 
   def index
-
-
     @links = Link.order('created_at DESC').paginate(:page => params[:page] ||= 1, :per_page => params[:per_page] ||= 25)
   end
 
