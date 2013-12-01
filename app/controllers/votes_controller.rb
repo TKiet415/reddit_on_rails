269a00f6a1d736a8bae7a5759bd7fd64544df3c9
Brian Pham <phambrian@live.com>
2013-11-30 23:17:20 -0800

    Part two of Reddit on Rails complete

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,9 @@
 class VotesController < ApplicationController
   def create
-    @vote = current_user.votes.create(params[:vote])
+    @vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first || current_user.votes.create(params[:vote])
+    @vote.update_attributes(:up => params[:vote][:up])
+    #puts "======================="
+    #puts @vote.errors.inspect
     redirect_to :back
   end
 end
